ab73fbb61e9f0116546c260fe0c91c14da53b059
Wali-Muhammad-1 <127096548+Wali-Muhammad-1@users.noreply.github.com>
2024-01-19 03:04:44 -0800

    Image is in swpping mode

--- src/app/Property-Binding/image-gallery/image-gallery.component.ts
+++ src/app/Property-Binding/image-gallery/image-gallery.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 export class ImageGalleryComponent {
   imageURL = 'https://images.unsplash.com/photo-1682687218608-5e2522b04673?q=80&w=1375&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
   imageTitle = 'Main Image'
+
+  
 }
